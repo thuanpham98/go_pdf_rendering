12b892f4f05e7ec5f84834fe5b8c04a6ecf3b771
thuanpm <thuanpm@unicloud.com.vn>
2022-07-19 18:45:21 +0700

    init

--- go.mod
+++ go.mod
@@ -3,21 +3,28 @@ module thuanpham98/go_pdf_rendering
 go 1.17
 
 require (
+	github.com/boombuler/barcode v1.0.1 // indirect
 	github.com/fatih/color v1.12.0 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/google/uuid v1.3.0 // indirect
 	github.com/hashicorp/go-hclog v1.2.0 // indirect
 	github.com/hashicorp/go-plugin v1.4.4 // indirect
 	github.com/hashicorp/yamux v0.0.0-20210707203944-259a57b3608c // indirect
+	github.com/hhrutter/lzw v0.0.0-20190829144645-6f07a24e8650 // indirect
+	github.com/hhrutter/tiff v0.0.0-20190829141212-736cae8d0bc7 // indirect
 	github.com/jolestar/go-commons-pool/v2 v2.1.2 // indirect
+	github.com/jung-kurt/gofpdf v1.16.2 // indirect
 	github.com/klippa-app/go-pdfium v1.1.0 // indirect
 	github.com/mattn/go-colorable v0.1.8 // indirect
 	github.com/mattn/go-isatty v0.0.13 // indirect
 	github.com/mitchellh/go-testing-interface v1.14.1 // indirect
 	github.com/oklog/run v1.1.0 // indirect
+	github.com/pdfcpu/pdfcpu v0.3.13 // indirect
 	// github.com/gen2brain/go-fitz v1.20.1 // indirect
 	// github.com/phpdave11/gofpdi v1.0.13 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
+	github.com/ruudk/golang-pdf417 v0.0.0-20201230142125-a7e3863a1245 // indirect
+	golang.org/x/image v0.0.0-20210220032944-ac19c3e999fb // indirect
 	// github.com/signintech/gopdf v0.13.0 // indirect
 	golang.org/x/mobile v0.0.0-20220518205345-8578da9835fd // indirect
 	golang.org/x/net v0.0.0-20220225172249-27dd8689420f // indirect
@@ -26,4 +33,5 @@ require (
 	google.golang.org/genproto v0.0.0-20210816143620-e15ff196659d // indirect
 	google.golang.org/grpc v1.40.0 // indirect
 	google.golang.org/protobuf v1.27.1 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
--- pdf_rendering.go
+++ pdf_rendering.go
@@ -1,72 +1,31 @@
 package pdfRendering
 
 import (
-	"image/png"
-	"log"
-	"os"
-	"time"
+	"bytes"
+	"fmt"
 
-	"github.com/klippa-app/go-pdfium"
-	// "github.com/klippa-app/go-pdfium/multi_threaded"
-	// "github.com/klippa-app/go-pdfium/multi_threaded"
-
-	"github.com/klippa-app/go-pdfium/requests"
-	"github.com/klippa-app/go-pdfium/single_threaded"
+	"github.com/pdfcpu/pdfcpu/pkg/pdfcpu"
 )
 
 type PdfDocument struct {
-	pool       pdfium.Pool
-	instance   pdfium.Pdfium
 	PageNums   int
 	PageImages string
 }
 
 func InitPdfRendering() *PdfDocument {
-	_pool := single_threaded.Init(single_threaded.Config{})
-
-	_instance, err := _pool.GetInstance(time.Second * 30)
-	if err != nil {
-		log.Fatal(err)
-	}
 	return &PdfDocument{
-		pool:       _pool,
-		instance:   _instance,
 		PageNums:   0,
 		PageImages: "",
 	}
 }
 
-func (pdfDocument *PdfDocument) ConvertPdfToImage(data []byte) error {
-
-	doc, _ := pdfDocument.instance.OpenDocument(&requests.OpenDocument{File: &data})
-	log.Fatal(doc.Document)
-	defer pdfDocument.instance.FPDF_CloseDocument(&requests.FPDF_CloseDocument{
-		Document: doc.Document,
-	})
-	res, err := pdfDocument.instance.RenderPageInDPI(&requests.RenderPageInDPI{
-		DPI: 72,
-		Page: requests.Page{
-			ByIndex: &requests.PageByIndex{
-				Document: doc.Document,
-				Index:    0,
-			},
-		},
-	})
-	if err != nil {
-		return err
+func (pdfDocument *PdfDocument) ConvertPdfToImage(data []byte) {
+	ctx, e := pdfcpu.Read(bytes.NewReader(data), &pdfcpu.Configuration{})
+	if e != nil {
+		fmt.Println(e)
 	}
-
-	// Write the output to a file.
-	f, err := os.Create("./example.pdf.png")
-	if err != nil {
-		return err
-	}
-	defer f.Close()
-
-	err = png.Encode(f, res.Result.Image)
-	if err != nil {
-		return err
-	}
-
-	return nil
+	fmt.Println(ctx)
+	// p , _ :=ctx.ExtractPage(0)
+	// p.Crop(p.LinearizationObjs,&pdfcpu.ImportBox{})
+	// pdfcpu.WMImage
 }
